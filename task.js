90c323f6e5a20c7bb07c32be6df018527a1ecbf8
vidhuran2002 <vidhuran2002@gmail.com>
2025-04-19 12:02:02 +0530

    first

---
+++ task.js
@@ -0,0 +1,38 @@
+function mouseoveranimation(){
+    let same=document.getElementById('same1')
+    same.classList.add("hover");
+   
+}
+
+
+
+function remove(){
+    let same=document.getElementById('same1')
+    
+same.classList.remove("hover");
+}
+
+function removeclass(){
+    const myTimeout = setTimeout(remove, 2000);
+}
+
+
+
+
+function mouseoveranimation2(){
+    let same=document.getElementById('same2')
+    same.classList.add("hover");
+   
+}
+
+
+
+function remove2(){
+    let same=document.getElementById('same2')
+    
+same.classList.remove("hover");
+}
+
+function removeclass2(){
+    const myTimeout = setTimeout(remove2, 2000);
+}
